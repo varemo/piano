8b23c75bb8f08360d8faf3eabff6c1f6fd2e2b52
Leif Varemo <piano.rpk@gmail.com>
2016-08-15 13:45:04 +0000

    Improved speed significantly of FDR calculation for GSEA in fdrGSEA().
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/piano@120138 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fdrGSEA.R
+++ R/fdrGSEA.R
@@ -13,6 +13,7 @@ fdrGSEA <- function(gsStatsAll,gsStatsAllPerm,nGenes,signMethod) {
       
       # For each gene-set (or gene-set size):
       randBgMatNorm <- randBgMat
+      
       for(iGeneSetSize in 1:nrow(randBgMat)) {
          
          # Normalize background:
@@ -28,59 +29,78 @@ fdrGSEA <- function(gsStatsAll,gsStatsAllPerm,nGenes,signMethod) {
       NES <- rep(NA,nrow(gsStatsAll))
       randBgMatNormFull <- as.data.frame(matrix(nrow=nrow(gsStatsAll),ncol=ncol(randBgMat)))
       
-      for(iGeneSet in 1:nrow(gsStatsAll)) {
-         
-         # Create whole background matrix (for all 'gene-sets' and permutations)
-         if(signMethod == "geneperm") {
-            sizeGS <- nGenes[iGeneSet,iContrast]
-            randBgMatNormFull[iGeneSet,] <- randBgMatNorm[as.character(sizeGS),]
-         } else {
-            randBgMatNormFull[iGeneSet,] <- randBgMatNorm[iGeneSet,]
-         }
-         
-         # Normalize ES:
-         ES <- gsStatsAll[iGeneSet,iContrast]
-         if(signMethod == "geneperm") {
-            tmp <- randBgMat[as.character(sizeGS),]
-         } else {
-            tmp <- randBgMat[iGeneSet,]
-         }
-         
-         #if(ES > 0 & max(tmp) <= 0) stop("normalization of enrichment scores failed, increase the number of permutations")
-         #else if(ES <= 0 & min(tmp) >= 0) stop("normalization of enrichment scores failed, increase the number of permutations")
-         
-         if(ES > 0) {
-            NES[iGeneSet] <- ES/max(c(mean(tmp[tmp >= 0]),0),na.rm=TRUE) # if no backgound, set mean to 0
-         } else if(ES < 0) {
-            NES[iGeneSet] <- ES/max(c(abs(mean(tmp[tmp <= 0])),0),na.rm=TRUE) # if no backgound, set mean to 0
-         } else {
-            NES[iGeneSet] <- 0  
-         }
+      # Create whole background matrix (for all 'gene-sets' and permutations)
+      if(signMethod == "geneperm") {
+          sizeGS <- nGenes[,iContrast]
+          randBgMatNormFull <- randBgMatNorm[as.character(sizeGS),]
+      } else {
+          randBgMatNormFull <- randBgMatNorm
       }
-       
+      
+      # Normalize ES:
+      ES <- gsStatsAll[, iContrast]
+      if(signMethod == "geneperm") {
+          tmp <- randBgMat[as.character(sizeGS),]
+      } else {
+          tmp <- randBgMat
+      }
+      
+      #if(ES > 0 & max(tmp) <= 0) stop("normalization of enrichment scores failed, increase the number of permutations")
+      #else if(ES <= 0 & min(tmp) >= 0) stop("normalization of enrichment scores failed, increase the number of permutations")
+    
+      {
+          iGeneSet <- which(ES > 0)          
+          NES[iGeneSet] <- ES[iGeneSet] / pmax(
+              apply(tmp[iGeneSet, ], 1, function(x) mean(x[x >= 0])),
+              0, 
+              na.rm=TRUE) # if no backgound, set mean to 0
+      }
+      
+      {
+          iGeneSet <- which(ES < 0)          
+          NES[iGeneSet] <- ES[iGeneSet] / pmax(
+              abs(apply(tmp[iGeneSet, ], 1, function(x) mean(x[x <= 0]))),
+              0, 
+              na.rm=TRUE) # if no backgound, set mean to 0
+      }
+      
+      NES[which(ES == 0)] <- 0
+      
+      # Negated for edgeCases of findInterval below
+      randBgNormUpNeg <- sort(-randBgMatNormFull[randBgMatNormFull >= 0])
+      randBgNormDn <- sort(randBgMatNormFull[randBgMatNormFull <= 0])
+      NES.up <- sort(-NES[NES >= 0])
+      NES.dn <- sort(NES[NES <= 0])
+      
+      
       # For each gene set:
       FDRup <- rep(NA,nrow(gsStatsAll))
       FDRdn <- rep(NA,nrow(gsStatsAll))
-      for(iGeneSet in 1:nrow(gsStatsAll)) {
-         
-         # Calculate FDR:
-         if(NES[iGeneSet] > 0) {
-            tmp1 <- sum(randBgMatNormFull >= NES[iGeneSet])/sum(randBgMatNormFull >= 0)
-            tmp2 <- sum(NES >= NES[iGeneSet])/sum(NES >= 0)
-            FDRup[iGeneSet] <- tmp1/tmp2
-            FDRdn[iGeneSet] <- NA
-         } else if(NES[iGeneSet] < 0) {
-            tmp1 <- sum(randBgMatNormFull <= NES[iGeneSet])/sum(randBgMatNormFull <= 0)
-            tmp2 <- sum(NES <= NES[iGeneSet])/sum(NES <= 0)
-            FDRdn[iGeneSet] <- tmp1/tmp2
-            FDRup[iGeneSet] <- NA
-         } else {
-            FDRup[iGeneSet] <- 1  
-            FDRdn[iGeneSet] <- 1
-         }
-         if(max(c(FDRup[iGeneSet],1), na.rm=TRUE) > 1) FDRup[iGeneSet] <- 1
-         if(max(c(FDRdn[iGeneSet],1), na.rm=TRUE) > 1) FDRdn[iGeneSet] <- 1
+      
+      {
+          iGeneSet <- which(NES > 0)
+          iGeneSet <- iGeneSet[order(-NES[iGeneSet])]
+          tmp1 <- findInterval(-NES[iGeneSet], randBgNormUpNeg) / length(randBgNormUpNeg)
+          tmp2 <- findInterval(-NES[iGeneSet], NES.up) / length(NES.up)
+          FDRup[iGeneSet] <- tmp1/tmp2
       }
+      
+      {
+          iGeneSet <- which(NES < 0)
+          iGeneSet <- iGeneSet[order(NES[iGeneSet])]
+          tmp1 <- findInterval(NES[iGeneSet], randBgNormDn) / length(randBgNormDn)
+          tmp2 <- findInterval(NES[iGeneSet], NES.dn) / length(NES.dn)
+          FDRdn[iGeneSet] <- tmp1/tmp2
+      }
+      
+      {
+          iGeneSet <- which(NES == 0)
+          FDRup[iGeneSet] <- 1  
+          FDRdn[iGeneSet] <- 1
+      }
+      
+      FDRup[which(FDRup > 1)] <- 1
+      FDRdn[which(FDRdn > 1)] <- 1
       
       # Save FDR in matrix:
       pValuesAllUpAdj <- cbind(pValuesAllUpAdj,FDRup)
